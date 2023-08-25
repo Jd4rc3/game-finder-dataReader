e383460323f847ae36c4c0f610a327977573ef44
Juan Daniel <juandanielarce398@gmail.com>
2023-08-25 10:29:38 -0500

    Add basic data to initial mongo data

--- init-mongo.js
+++ init-mongo.js
@@ -1,28 +1,13 @@
 db = db.getSiblingDB('Parameters');
-db.parameters.createIndex({Name: 1}, {unique: true});
-db.parameters.insertMany([{
-    "name": "platforms", "values": [{
-        "name": "eneba", "commonName": "xbox", "valueName": "xbox"
-    }, {
-        "name": "xbox", "commonName": "xbox", "valueName": "xboxseries"
-    }]
-}, {
-    "name": "regions", "values": [{
-        "name": "eneba", "commonName": "colombia", "valueName": "colombia"
-    }]
-}]);
-// {
-//     "name": "<string>",
-//     "values": [
-//     {
-//         "name": "<string>",
-//         "commonName": "<string>",
-//         "valueName": "<string>"
-//     },
-//     {
-//         "name": "<string>",
-//         "commonName": "<string>",
-//         "valueName": "<string>"
-//     }
-// ]
-// }
+db.parameters.createIndex({ Name: 1 }, { unique: true });
+db.parameters.insertMany([
+    {
+        "Name": "platforms", "Values": [
+            { "Name": "eneba", "CommonName": "xbox", "ValueName": "xbox" },
+            { "Name": "xbox", "CommonName": "xbox", "ValueName": "xboxseries" }
+        ]
+    },
+    {
+        "Name": "regions", "Values": [{ "Name": "eneba", "CommonName": "colombia", "ValueName": "colombia" }]
+    }
+]);
