3e3d4bbc70a97f40f53a6ab5a709862992138b66
Juan Daniel <juandanielarce398@gmail.com>
2023-08-24 23:35:20 -0500

    Common name attribute to store param

--- Domain/Models/Store.cs
+++ Domain/Models/Store.cs
@@ -3,6 +3,8 @@ namespace Domain.Models
     public class Store
     {
         public string Name { get; set; }
+        
+        public string commonName { get; set; }
 
         public string ValueName { get; set; }
     }
