2cb234d689aaf10a98d98ed5ffe2d66a54887445
Jd4rc3 <103149971+Jd4rc3@users.noreply.github.com>
2023-08-25 09:55:08 -0500

    Feature/initial db fix username & image +patch (#1)
    * Initial data
    
    * lightweight tags fix
    
    * Fix checkout depth
    
    * fix user and image names
    
    ---------
    
    Co-authored-by: Juan Daniel <juandanielarce398@gmail.com>

--- init-mongo.js
+++ init-mongo.js
@@ -1,2 +1,28 @@
 db = db.getSiblingDB('Parameters');
 db.parameters.createIndex({Name: 1}, {unique: true});
+db.parameters.insertMany([{
+    "name": "platforms", "values": [{
+        "name": "eneba", "commonName": "xbox", "valueName": "xbox"
+    }, {
+        "name": "xbox", "commonName": "xbox", "valueName": "xboxseries"
+    }]
+}, {
+    "name": "regions", "values": [{
+        "name": "eneba", "commonName": "colombia", "valueName": "colombia"
+    }]
+}]);
+// {
+//     "name": "<string>",
+//     "values": [
+//     {
+//         "name": "<string>",
+//         "commonName": "<string>",
+//         "valueName": "<string>"
+//     },
+//     {
+//         "name": "<string>",
+//         "commonName": "<string>",
+//         "valueName": "<string>"
+//     }
+// ]
+// }
